ed4c307b1cc9d5b9e5154973e37f980d88e70ea9
Josh Chase <jcjoshuachase@gmail.com>
2014-10-21 18:24:24 -0400

    from dev 2

---
+++ test2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, world!\n");
+	printf("Drunk coding ftw!\n");
+	printf("I love git.\n");
+	printf("not yet staged.\n");
+
+	printf("More master changes\n");
+	printf("This is on the new branch!");
+}
