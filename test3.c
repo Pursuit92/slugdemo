9a861846dd32dce14e1c44909b096f064d696894
Josh Chase <jcjoshuachase@gmail.com>
2014-10-21 18:26:59 -0400

    clay is dumb

---
+++ test3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, world!\n");
+	printf("Number two\n");
+	printf("Number three\n");
+	printf("I love git.\n");
+	printf("not yet staged.\n");
+
+	printf("More master changes\n");
+	printf("This is on the new branch!");
+}
